f1dd5509104bb8f82a981cdd634abe6feefbb2b7
Ecthor Silva <EcthorSilva@outlook.com>
2023-04-24 13:47:00 -0300

    Feature: Atualização no sistema de ataque dos personagens

--- src/Mago.java
+++ src/Mago.java
@@ -5,20 +5,32 @@ public class Mago extends Personagem {
         super(nome, arma, vida, forca, defesa, destreza, estusFlask);
     }
 
+    Random rand = new Random();
+
     @Override
     public int atacar() {
-        Random rand = new Random();
         int min = getForca() / 2;
         int max = getForca() + getDefesa();
-        int dano = rand.nextInt(max - min + 1) + min;
-        System.out.printf("Você lançou um feitiço com o seu %s e causou %d de dano!\n", getArma(), dano);
+        int dano = 0;
+
+        if(rand.nextDouble() < 0.85){ // 85% de chances de acertar e 15% de errar
+            if(rand.nextDouble() < 0.15){ // Ataque critico (15% de chances de acerto critico)
+                dano = (rand.nextInt(max - min + 1) + min) * 2;
+                System.out.printf("Você lançou um feitiço com o seu %s e causou %d de dano CRITICO! \n", getArma(), dano);
+            }else{ // Ataque normal
+                dano = rand.nextInt(max - min + 1) + min;
+                System.out.printf("Você lançou um feitiço com o seu %s e causou %d de dano!\n", getArma(), dano);
+            }
+        }else{ // errou
+            System.out.printf("Você errou o ataque!\n");
+        }
         return dano;
     }
     
     @Override
     public int fugir() {
         double chanceDeFuga = getDestreza() * 0.05;
-        if (Math.random() <= chanceDeFuga) {
+        if (rand.nextDouble() <= chanceDeFuga) {
             System.out.printf("Você conseguiu fugir da batalha!\n\n");
             return 1;
         } else {
